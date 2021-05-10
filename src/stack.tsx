4f64553f9c54e9e24471fb2738773a6370adbb6f
bfu4 <bella@sleeeepyti.me>
2021-05-10 03:24:05 -0400

    make it more evident

--- src/stack.tsx
+++ src/stack.tsx
@@ -17,19 +17,20 @@ import {
 const ICON_SIZE = 30;
 
 const items = [
-    {id: "java", content: <SiJava size={ICON_SIZE} className="icon"/>},
-    {id: "cpp", content: <SiCplusplus size={ICON_SIZE} className="icon"/> },
-    {id: "best-shell", content: <SiGnubash size={ICON_SIZE} className="icon"/>},
-    {id: "node", content: <SiNodeDotJs size={ICON_SIZE} className="icon"/>},
-    {id: "hell", content: <SiV8 size={ICON_SIZE} className="icon" />},
-    {id: "best-text-editor", content: <SiVim size={ICON_SIZE} className="icon" />},
-    {id: "java-best-editor", content: <SiIntellijidea size={ICON_SIZE} className="icon" />},
-    {id: "maven", content: <SiApachemaven size={ICON_SIZE} className="icon"/>},
-    {id: "git1", content: <SiGithub size={ICON_SIZE} className="icon" />},
-    {id: "git2", content: <SiGitlab size={ICON_SIZE} className="icon" />},
-    {id: "git3", content: <SiGit size={ICON_SIZE} className="icon" />},
-    {id: "ps", content: <SiAdobephotoshop size={ICON_SIZE} className="icon" />},
+    {id: "java", content: <SiJava title="Java" size={ICON_SIZE} className="icon"/>},
+    {id: "cpp", content: <SiCplusplus title="C++" size={ICON_SIZE} className="icon"/> },
+    {id: "best-shell", content: <SiGnubash title="Bash" size={ICON_SIZE} className="icon"/>},
+    {id: "node", content: <SiNodeDotJs title="Node.js" size={ICON_SIZE} className="icon"/>},
+    {id: "hell", content: <SiV8 title="V8" size={ICON_SIZE} className="icon" />},
+    {id: "best-text-editor", content: <SiVim title="vim" size={ICON_SIZE} className="icon" />},
+    {id: "java-best-editor", content: <SiIntellijidea title="IntelliJ" size={ICON_SIZE} className="icon" />},
+    {id: "maven", content: <SiApachemaven title="maven" size={ICON_SIZE} className="icon"/>},
+    {id: "git1", content: <SiGithub title="Github" size={ICON_SIZE} className="icon" />},
+    {id: "git2", content: <SiGitlab title="Gitlab" size={ICON_SIZE} className="icon" />},
+    {id: "git3", content: <SiGit title="Git" size={ICON_SIZE} className="icon" />},
+    {id: "ps", content: <SiAdobephotoshop title="Photoshop" size={ICON_SIZE} className="icon" />},
     {id: "sol", content: <img
+            title="Solidity"
             src="https://raw.githubusercontent.com/bfu4/bfu4/master/sol.png"
             alt="sol"
             width={ICON_SIZE}
